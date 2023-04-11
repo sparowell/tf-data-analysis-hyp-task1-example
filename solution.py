e5c6067133ceedd4548f462121fb313ea9589b73
sparowell <103679474+sparowell@users.noreply.github.com>
2023-04-11 19:31:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import numpy as np
+from scipy import stats as st
 
+chat_id = 654929803 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(m1: int, 
+             n1: int, 
+             m2: int, 
+             n2: int) -> bool:
+    p1 = m1/n1
+    p2 = m2/n2
+    df = n1 + n2 - 2
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    var1 = p1*(1-p1)
+    var2 = p2*(1-p2)
+    sme = var1/n1 + var2/n2
+
+    t = (p1-p2)/np.sqrt(sme)
+    p = st.t.cdf(t,df)
+    return p<0.05
